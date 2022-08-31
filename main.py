b0d89661369f7e3af7bd74fd34233f3971c274af
Bebebole <79281901+Bebebole@users.noreply.github.com>
2022-08-31 22:55:05 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from time import sleep
+from subprocess import check_output
+from base64 import b64decode
+import platform
+
+def getChromedriverPath() :
+    if platform.system() == 'Windows' :
+        locateCmd = 'where'
+    else :
+        locateCmd = 'whereis'
+
+    chromedriverPath = check_output([locateCmd, 'chromedriver']).strip().decode('utf-8')
+
+    if 'INFO:' in chromedriverPath or chromedriverPath == 'chromedriver:' :
+        print('Chromedriver isn\'t installed..')
+        exit()
+    else :
+        if platform.system() != 'Windows' :
+            chromedriverPath = chromedriverPath.replace('chromedriver: ', '')
+        else :
+            pass
+    
+    return chromedriverPath
+
+def clickButton(id) :
+    try:
+        bail = WebDriverWait(browser, 4).until(
+            EC.presence_of_element_located((By.ID, id))
+        )
+    finally :
+        bail.click()
+
+username = 'user'
+password = 'pass'
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--headless")
+s = Service(getChromedriverPath())
+browser = webdriver.Chrome(service=s, options=options)
+browser.get('https://192.168.1.1/')
+
+usernameBox = browser.find_element('id', 'Frm_Username')
+passwordBox = browser.find_element('id', 'Frm_Password')
+loginButton = browser.find_element('id', 'LoginId')
+
+usernameBox.send_keys(username)
+passwordBox.send_keys(password)
+loginButton.click()
+
+clickButton('mgrAndDiag')
+clickButton('devMgr')
+clickButton('Btn_restart')
+clickButton('confirmOK')
+sleep(7)
+
+browser.quit()
+exit()
