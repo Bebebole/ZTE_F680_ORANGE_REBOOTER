a2e2adf93796d6cd704a038380baee1dca06aa3f
Bebebole <79281901+Bebebole@users.noreply.github.com>
2022-08-31 23:02:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
-from subprocess import check_output
 
 def clickButton(id) :
     try:
